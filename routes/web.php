c9a9eec254c2849bc5bf03b7ba1dc353fb5fda2f
Forhad-Hossain <pycoder028@gmail.com>
2024-07-15 05:29:01 +0600

    Multi User Auth Setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +31,11 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth','role:admin'])->group(function(){
+    Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard'])->name('admin.dashboard');
+}); // end group admin middleware
+
+Route::middleware(['auth','role:agent'])->group(function(){
+    Route::get('/agent/dashboard',[AgentController::class,'AgentDashboard'])->name('agent.dashboard');
+}); // end group agent middleware
