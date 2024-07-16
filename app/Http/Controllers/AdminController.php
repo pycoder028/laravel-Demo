d997bce511ffbbf505c2abc7e58d90b76bf77d7e
Forhad-Hossain <pycoder028@gmail.com>
2024-07-16 19:22:58 +0600

    Change admin password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -68,6 +69,48 @@ class AdminController extends Controller
         return redirect()->back()->with($notification);
 
     }   // end method
+
+    public function AdminChangePassword(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+
+        return view('admin.pages.admin_change_password',compact('profileData'));
+    }   // end method
+
+    public function AdminUpdatePassword(Request $request){
+
+        // Validation
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        // Match the old password
+        if(!Hash::check($request->old_password, auth::user()->password)){
+
+            $notification = array(
+                'message' => 'Old Password Does not Match!',
+                'alert-type' => 'error'
+            );
+
+            return back()->with($notification);
+        }
+
+        // Update the new password
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        $notification = array(
+            'message' => 'Password Change Successfully',
+            'alert-type' => 'success'
+        );
+
+        return back()->with($notification);
+
+    }   // end method
+
 
 
 }
--- routes/web.php
+++ routes/web.php
@@ -34,14 +34,20 @@ require __DIR__.'/auth.php';
 
 // admin group middleware
 Route::middleware(['auth','role:admin'])->group(function(){
+
+
     Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout',[AdminController::class,'AdminLogout'])->name('admin.logout');
     Route::get('/admin/profile',[AdminController::class,'AdminProfile'])->name('admin.profile');
     Route::post('/admin/profile/store',[AdminController::class,'AdminProfileStore'])->name('admin.profile.store');
+    Route::get('/admin/change/password',[AdminController::class,'AdminChangePassword'])->name('admin.change.password');
+    Route::post('/admin/update/password',[AdminController::class,'AdminUpdatePassword'])->name('admin.update.password');
+
+
 }); // end group admin middleware
 
 
-// admin group middleware
+// agent group middleware
 Route::middleware(['auth','role:agent'])->group(function(){
     Route::get('/agent/dashboard',[AgentController::class,'AgentDashboard'])->name('agent.dashboard');
 }); // end group agent middleware
